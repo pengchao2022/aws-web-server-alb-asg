115b644844a4659f509b46a85544a36e084a4654
pengchao <pengchao.ma2@outlook.com>
2025-09-22 19:11:21 +0800

    modified again

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
